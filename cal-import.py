b7e36e1648c05a66b0b068cc89b2c820ffc51078
David <49169805+mesub7@users.noreply.github.com>
2025-09-08 13:21:46 +0100

    Add date ranges

--- cal-import.py
+++ cal-import.py
@@ -1,6 +1,11 @@
+import json
 import requests
+import os
 
 with requests.Session() as s:
-    s.post('https://my.lboro.ac.uk/campusm/sso/ldap/2548', data={'username': 'xxx', 'password': 'xxx'})
-    x1 = s.get('https://my.lboro.ac.uk/campusm/sso/cal2/course_timetable?start=2025-08-31T23%3A00%3A00.000Z&end=2025-10-31T23%3A59%3A59.000Z')
-    print(x1.text)
+    s.post('https://my.lboro.ac.uk/campusm/sso/ldap/2548', data={'username': os.environ['LboroUser'], 'password': os.environ['LboroPass']})
+    x1 = s.get('https://my.lboro.ac.uk/campusm/sso/cal2/course_timetable?start=2025-09-29T23%3A00%3A00.000Z&end=2025-12-01T23%3A59%3A59.000Z')
+    x2 = s.get('https://my.lboro.ac.uk/campusm/sso/cal2/course_timetable?start=2025-12-02T23%3A00%3A00.000Z&end=2026-02-19T23%3A59%3A59.000Z')
+    x3 = s.get('https://my.lboro.ac.uk/campusm/sso/cal2/course_timetable?start=2026-02-20T23%3A00%3A00.000Z&end=2026-05-02T23%3A59%3A59.000Z')
+    x4 = s.get('https://my.lboro.ac.uk/campusm/sso/cal2/course_timetable?start=2026-05-03T23%3A00%3A00.000Z&end=2026-06-13T23%3A59%3A59.000Z')
+    print(x4.text)
