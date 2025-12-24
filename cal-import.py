3334cf10fb9d704ddbd8b53fb3861185f03cb38a
David Oladiran <daveanddan70@gmail.com>
2025-12-24 17:07:02 +0000

    Update to V2

--- cal-import.py
+++ cal-import.py
@@ -4,23 +4,21 @@ import requests
 import os
 import icalendar
 
+OUTPUT_PATH = '...'  # Fill this out
+
 cal = icalendar.Calendar()
-cal.calendar_name = "Computer Science Timetable - David Oladiran"
+cal.calendar_name = "Timetable"  # May want to change to suit
 cal.add('prodid', '-//mesub7//Lboro-Cal//')
 cal.add('version', '2.0')
 cal.add('calscale', 'GREGORIAN')
 
 with requests.Session() as s:
-    s.post('https://my.lboro.ac.uk/campusm/sso/ldap/2548', data={'username': os.environ['LboroUser'], 'password': os.environ['LboroPass']})
-    x1 = json.loads(s.get('https://my.lboro.ac.uk/campusm/sso/cal2/course_timetable?start=2025-09-29T23%3A00%3A00.000Z&end=2025-10-04T23%3A59%3A59.000Z').text)
-    period1 = json.loads(s.get('https://my.lboro.ac.uk/campusm/sso/cal2/course_timetable?start=2025-09-29T23%3A00%3A00.000Z&end=2025-12-01T23%3A59%3A59.000Z').text)
+    s.post('https://my.lboro.ac.uk/campusm/sso/ldap/2548', data={'username': os.environ['LBOROUSER'], 'password': os.environ['LBOROPASS']})
+    period1 = json.loads(s.get('https://my.lboro.ac.uk/campusm/sso/cal2/course_timetable?start=2025-09-28T23%3A00%3A00.000Z&end=2025-12-01T23%3A59%3A59.000Z').text)
     period2 = json.loads(s.get('https://my.lboro.ac.uk/campusm/sso/cal2/course_timetable?start=2025-12-02T23%3A00%3A00.000Z&end=2026-02-19T23%3A59%3A59.000Z').text)
     period3 = json.loads(s.get('https://my.lboro.ac.uk/campusm/sso/cal2/course_timetable?start=2026-02-20T23%3A00%3A00.000Z&end=2026-05-02T23%3A59%3A59.000Z').text)
     period4 = json.loads(s.get('https://my.lboro.ac.uk/campusm/sso/cal2/course_timetable?start=2026-05-03T23%3A00%3A00.000Z&end=2026-06-13T23%3A59%3A59.000Z').text)
 
-print(x1['events'][0]['eventRef'])
-
-# print(period1['events'])
 periods = [period1, period2, period3, period4]
 for period in periods:
     for i in range(len(period['events'])):
@@ -33,6 +31,10 @@ for period in periods:
             teaching['teacherEmail']
         except KeyError:
             teaching['teacherEmail'] = None
+        if teaching['teacherEmail']:
+            teachers = teaching['teacherEmail'].split(';')
+        else:
+            teachers = "anonymous@lboro.ac.uk"
         event = icalendar.Event()
         event.add('uid', teaching['eventRef'])
         event.add('dtstamp', datetime.now())
@@ -41,22 +43,11 @@ for period in periods:
         event.add('summary', teaching['desc1'])
         event.add('description', f"{teaching['desc1']} with {teaching['teacherName']}." if teaching['teacherName'] else f"{teaching['desc1']}.")
         event.add('location', teaching['locAdd1'])
-        event.add('organiser', f"mailto:{teaching['teacherEmail']}" if teaching['teacherEmail'] else "mailto:anonymous@lboro.ac.uk")
+        for teacher in teachers:
+            event.add('organizer', f"mailto:{teacher}")
         cal.add_component(event)
 
-with open('test_cal.ics', 'wb') as f:
+tmp_path = OUTPUT_PATH + ".tmp"
+with open(tmp_path, "wb") as f:
     f.write(cal.to_ical())
-
-# period1['events']... - get len and interate through
-# Get the relevant details: uid -> eventref, dtstamp -> now, dtstart/end -> start/end
-# summary -> desc1, description - F STRING of {desc1} with {teacherName} location -> locAdd1, organiser -> mailto:teacherEmail
-# populate to calendar
-# save
-
-# Find a way to host on a server (oracle!?!?)
-
-# See if outlook will accept it
-
-# Do batch jobs - update every day at midnight
-
-# Advance stuff - overwrite the calendar every day.
+os.replace(tmp_path, OUTPUT_PATH)
